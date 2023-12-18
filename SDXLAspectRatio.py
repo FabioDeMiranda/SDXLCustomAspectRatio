d3a2465e69b4a2bbdeaf0e520a6d595f9e4d5375
FabioDeMiranda <FabioDeMiranda@users.noreply.github.com>
2023-12-18 18:00:06 +0100

    Improvements

--- SDXLAspectRatio.py
+++ SDXLAspectRatio.py
@@ -1,3 +1,4 @@
+import re
 # quick node to set SDXL-friendly aspect ratios in 1024^2
 # by throttlekitty
 
@@ -9,9 +10,14 @@ class SDXLAspectRatio:
     def INPUT_TYPES(s):
         return {
                 "required": {
-                    "width": ("INT", {"default": 64, "min": 64, "max": 2048,}),
-                    "height": ("INT", {"default": 64, "min": 64, "max": 2048}),
+                    "width": ("INT", {"default": 512, "min": 64, "max": 2048,}),
+                    "height": ("INT", {"default": 512, "min": 64, "max": 2048}),
                     "aspectRatio": ([
+                    "1:1  - 512x512 square",
+                    "1:1  - 640x640 square",
+                    "1:1  - 768x768 square",
+                    "2:3  - 512x768 square",
+                    "3:2  - 768x512 square",
                     "1:1  - 1024x1024 square", 
                     "2:3  - 832x1216 portrait", 
                     "3:4  - 896x1152 portrait", 
@@ -33,33 +39,11 @@ class SDXLAspectRatio:
     CATEGORY = "image"
 
     def SDXL_AspectRatio(self, width, height, aspectRatio):
-        if aspectRatio == "1:1  - 1024x1024 square":
-            width, height = 1024, 1024
-        elif aspectRatio == "2:3  - 832x1216 portrait":
-            width, height = 832, 1216
-        elif aspectRatio == "3:4  - 896x1152 portrait":
-            width, height = 896, 1152
-        elif aspectRatio == "5:8  - 768x1216 portrait":
-            width, height = 768, 1216
-        elif aspectRatio == "9:16 - 768x1344 portrait":
-            width, height = 768, 1344
-        elif aspectRatio == "9:19 - 704x1472 portrait":
-            width, height = 704, 1472
-        elif aspectRatio == "9:21 - 640x1536 portrait":
-            width, height = 640, 1536
-        elif aspectRatio == "3:2  - 1216x832 landscape":
-            width, height = 1216, 832
-        elif aspectRatio == "4:3  - 1152x896 landscape":
-            width, height = 1152, 896
-        elif aspectRatio == "8:5  - 1216x768 landscape":
-            width, height = 1216, 768
-        elif aspectRatio == "16:9 - 1344x768 landscape":
-            width, height = 1344, 768
-        elif aspectRatio == "19:9 - 1472x704 landscape":
-            width, height = 1472, 704
-        elif aspectRatio == "21:9 - 1536x640 landscape":
-            width, height = 1536, 640
-        return(width, height)
+        pattern = r"(\d+):(\d+)\s+-\s+(\d+)x(\d+)\s+(.*)"
+        match = re.match(pattern, aspectRatio)
+        if match:
+            width, height = int(match.group(3)), int(match.group(4))
+        return width, height
 
             
 NODE_CLASS_MAPPINGS = {
